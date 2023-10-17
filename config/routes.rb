6261401d679de460a4e32471e0d963ca5c14f020
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-16 21:36:08 -0600

    Set up initial route for User in order to implement login and sign in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+  resources :users, only: [:index, :show] do
 end
