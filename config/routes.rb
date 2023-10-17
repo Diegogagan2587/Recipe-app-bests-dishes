06d2ec70b7fbf14ad508b0843d0a9dffee9d7897
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-17 14:06:01 -0600

    Add routes for creating new food

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
   resources :users, only: [:index, :show]
-  resources :foods, only: [:index, :show]
+  resources :foods, only: [:index, :show, :new, :create]
 end
