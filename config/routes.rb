3d3b71c25d32ba0f30fed52756c42fb741b4362d
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-10-18 01:47:10 +0300

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
+  get '/home', to: 'home#index'
   resources :users, only: [:index, :show]
+  resources :recipes, only: %i[index show new create destroy]
+  get '/public_recipe', to: 'recipes#public_recipe', as: 'public_recipe'
 end
