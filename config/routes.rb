172cd0a97deb78e46977c6e9f052714659b703c6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-18 11:09:08 -0600

    Add route for delete_food action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
   resources :users, only: [:index, :show]
-  resources :foods, only: [:index, :show, :new, :create]
+  resources :foods, only: [:index, :show, :new, :create] do
+    delte 'delete_food', on: :member
+  end
 end
