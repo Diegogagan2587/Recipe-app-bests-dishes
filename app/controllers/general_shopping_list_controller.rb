4014092ffeb9afce7542355a1c064f42e90dfd52
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-19 01:42:40 -0600

    Show list of foods that are missing from the foods page

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,2 +1,16 @@
 class GeneralShoppingListController < ApplicationController
+  def index
+    @recipefoods = current_user.recipe_foods
+    @shopping_list = Hash.new { |hash, key| hash[key] = { quantity: 0, price: 0 } }
+    @recipefoods.each do |recipefood|
+      food = Food.find(recipefood.food_id)
+      if food.quantity.zero?
+        @shopping_list[food.name][:quantity] += recipefood.quantity
+        @shopping_list[food.name][:price] += food.price * @shopping_list[food.name][:quantity]
+      elsif food.quantity < recipefood.quantity
+        @shopping_list[food.name][:quantity] += recipefood.quantity - food.quantity
+        @shopping_list[food.name][:price] += food.price * @shopping_list[food.name][:quantity]
+      end
+    end
+  end
 end
