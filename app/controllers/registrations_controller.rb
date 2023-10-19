44e880d2a6fd6c62eef995a9ab1e3722b6772068
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-18 21:16:13 -0600

    Permit Name for registration from

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,8 @@
 class RegistrationController < Devise::RegistrationsController
+    before_action :configure_permitted_parameters
+
+    protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
