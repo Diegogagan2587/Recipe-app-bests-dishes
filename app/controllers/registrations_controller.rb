b40556116c31d4eddfad4f94b658453b5a7bbd92
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-18 21:27:52 -0600

    Fix rubocop

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,9 @@
-class RegistrationController < Devise::RegistrationsController
-    before_action :configure_permitted_parameters
+class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
 
-    protected
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
