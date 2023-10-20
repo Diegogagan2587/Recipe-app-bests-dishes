b4a28e507168dbf7b7b340b589fa035ec6d379d1
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-19 18:28:53 -0600

    Fix Rubocop

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
-  #we need to validate the presence of the name, measurement_unit, price and quantity attributes
+  # we need to validate the presence of the name, measurement_unit, price and quantity attributes
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true
